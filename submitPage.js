4687d1edf7df6f94b88a843dc312ddaa77e02d58
Khai281298 <khailuong61@gmail.com>
2021-07-05 21:47:38 +0700

    #210705: fix timezone

--- submitPage.js
+++ submitPage.js
@@ -23,7 +23,7 @@ const findSurveySubmit = async (sId, sAuthor, socket) => {
         .exec()
         .then((data) => {
             var checkTime = true;
-            var date = new Date(formatDate()).getTimezoneOffset();
+            var date = new Date(formatDate());
             console.log(date);
 
             if (data.timer.start.status && data.timer.end.status) {
